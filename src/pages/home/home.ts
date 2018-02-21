6d53718021c2873bf7895eea78224039cb9211de
Jakub Berka <berkajak@gmail.com>
2018-02-21 19:34:55 +0100

    j

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,11 +20,12 @@ export class HomePage {
   route: any;
   activeSegment: any;
   activeSegmentIndex: number;
-
+  nextBeacon: object;
 
   constructor(private change: ChangeDetectorRef, private vibration: Vibration, private tts: TextToSpeech, private platform: Platform,
     public navCtrl: NavController, public beaconProvider: BeaconProvider, public events: Events) {
     this.zone = new NgZone({ enableLongStackTrace: false });
+    // possible error if first segment has undefined endbeacon
 
     this.route = {
       routeDescription: "Trasa z adresy Karlovo náměstí 293/13  na adresu Karlovo náměstí 293/13 do učebny Ká 328 v budově E. Trasa je dlouhá asi 1000 metrů a vede přes 5 přechodů. Součástí trasy je cesta tramvají ze zastávky Jiráskovo náměstí na zastávku Myslíkova. Postav se tak, abys měl budovy za zády.",
@@ -53,28 +54,37 @@ export class HomePage {
           description: "Úzká rovná chodba asi 10 m dlouhá, před koncem chodby většinou otevřené dvojkřídlé skleněné dveře s madlem.",
           action: "Jdi chodbou ke dveřím a projdi.",
           startBeacon: "vf3i",
-          endBeacon: "y4Wl"
+          endBeacon: "y4rk"
 
         }
       ],
       routeBeacons: [
-        { id: "4vXL", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 12345, minor: 3775 },
-        { id: "ODUM", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 13796, minor: 64969 },
-        { id: "vf3i", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 999, minor: 300 },
-        { id: "y4Wl", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 2000, minor: 202 }
+        { id: "4vXL", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 1000, minor: 200 },
+        { id: "ODUM", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 1000, minor: 100 },
+        { id: "vf3i", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 1000, minor: 300 },
+        { id: "y4rk", uuid: "f7826da6-4fa2-4e98-8024-bc5b71e0893e", major: 1000, minor: 400 }
       ]
     }
 
     this.activeSegmentIndex = 0;
     this.activeSegment = this.route.segments[this.activeSegmentIndex];
+    this.setNextBeacon();
+    this.startScan(this.nextBeacon);
+  }
 
+  // refactor
+  setNextBeacon() {
+    var nextBeaconId = this.route.segments[this.activeSegmentIndex].endBeacon;
+    this.nextBeacon = this.route.routeBeacons.filter(function (beacon) { return beacon.id == nextBeaconId })[0];
   }
 
   nextSegment() {
-    if(this.activeSegmentIndex < this.route.segments.length - 1){
+    if (this.activeSegmentIndex < this.route.segments.length - 1) {
       this.activeSegmentIndex++;
     }
     this.activeSegment = this.route.segments[this.activeSegmentIndex];
+    this.setNextBeacon();
+    this.startScan(this.nextBeacon);
   }
 
 
@@ -83,38 +93,41 @@ export class HomePage {
       this.activeSegmentIndex--;
     }
     this.activeSegment = this.route.segments[this.activeSegmentIndex];
+    this.setNextBeacon();
+    this.startScan(this.nextBeacon);
   }
 
-  startScan() {
+  startScan(beacon) {
     this.platform.ready().then(() => {
-      this.beaconProvider.initialise(this.route.routeBeacons[0]).then((isInitialised) => {
+      this.beaconProvider.initialise(beacon).then((isInitialised) => {
         if (isInitialised) {
-
           this.listenToBeaconEvents();
-
 
         }
       });
     });
-
-
 
   }
 
 
 
   listenToBeaconEvents() {
-    this.events.subscribe('didRangeBeaconsInRegion', (data) => {
+    this.events.subscribe('didEnterRegion', (data) => {
       console.log(data);
       this.zone.run(() => {
         this.beaconsData = data.beacons;
-        console.log(this.beaconsData);
+
         this.beaconsData.forEach((beacon) => {
           console.log(beacon);
-          if (beacon.major == 12345 && beacon.minor == 3775) {
-            if (beacon.proximity == 'ProximityImmediate') {
-              this.vibration.vibrate(100);
+          if (beacon.proximity == "ProximityImmediate") {
+            var ttsOptions = {
+              text: "Jsi blízko",
+              locale: "cs-CZ"
             }
+            this.tts.speak(ttsOptions)
+              .then(() => console.log('Success'))
+              .catch((reason: any) => console.log(reason));
+            this.vibration.vibrate(100);
           }
 
         });
